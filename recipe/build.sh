59189be11d23f447e1a4dcba915fafb1f3534e1f
Nick Santos <nicholas.j.santos@gmail.com>
2020-07-31 12:12:25 -0400

    update tilt-feedstock to 0.17.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 if [[ $(uname) = "Darwin" ]]; then
     export CGO_ENABLED=1
 fi
 
-VERSION=0.16.1
-COMMIT=1f21e296d6a1029223d273bcf43dc6b0bf71c0e8
-go install -tags=osusergo -mod=vendor -ldflags="-s -w -X main.version=$VERSION -X main.commit=$COMMIT" ./cmd/tilt
+DATE=$(date +"%Y-%m-%d")
+go install -tags=osusergo -mod=vendor -ldflags="-s -w -X main.version=$PKG_VERSION -X main.date=$DATE" ./cmd/tilt
 
 bash $RECIPE_DIR/build_library_licenses.sh
